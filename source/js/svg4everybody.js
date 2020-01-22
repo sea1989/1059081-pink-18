e3808cf5d1f982877402497d9d0edb0455effc34
Виктор <vicvilkov@yandex.ru>
2020-01-22 23:58:08 +0300

    защита

--- source/js/svg4everybody.js
+++ source/js/svg4everybody.js
@@ -1,12 +1,12 @@
-!function(root, factory) {
+!function (root, factory) {
   "function" == typeof define && define.amd ? // AMD. Register as an anonymous module unless amdModuleId is set
-  define([], function() {
+    define([], function () {
       return root.svg4everybody = factory();
-  }) : "object" == typeof module && module.exports ? // Node. Does not work with strict CommonJS, but
-  // only CommonJS-like environments that support module.exports,
-  // like Node.
-  module.exports = factory() : root.svg4everybody = factory();
-}(this, function() {
+    }) : "object" == typeof module && module.exports ? // Node. Does not work with strict CommonJS, but
+      // only CommonJS-like environments that support module.exports,
+      // like Node.
+      module.exports = factory() : root.svg4everybody = factory();
+}(this, function () {
   /*! svg4everybody v2.1.9 | github.com/jonathantneal/svg4everybody */
   function embed(parent, svg, target) {
     // if the target exists
@@ -17,8 +17,8 @@
       viewBox && svg.setAttribute("viewBox", viewBox);
       // copy the contents of the clone into the fragment
       for (// clone the target
-      var clone = target.cloneNode(!0); clone.childNodes.length; ) {
-          fragment.appendChild(clone.firstChild);
+        var clone = target.cloneNode(!0); clone.childNodes.length;) {
+        fragment.appendChild(clone.firstChild);
       }
       // append the fragment into the svg
       parent.appendChild(fragment);
@@ -26,35 +26,35 @@
   }
   function loadreadystatechange(xhr) {
     // listen to changes in the request
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       // if the request is ready
       if (4 === xhr.readyState) {
         // get the cached html document
         var cachedDocument = xhr._cachedDocument;
         // ensure the cached html document based on the xhr response
         cachedDocument || (cachedDocument = xhr._cachedDocument = document.implementation.createHTMLDocument(""),
-        cachedDocument.body.innerHTML = xhr.responseText, xhr._cachedTarget = {}), // clear the xhr embeds list and embed each item
-        xhr._embeds.splice(0).map(function(item) {
-          // get the cached target
-          var target = xhr._cachedTarget[item.id];
-          // ensure the cached target
-          target || (target = xhr._cachedTarget[item.id] = cachedDocument.getElementById(item.id)),
-          // embed the target into the svg
-          embed(item.parent, item.svg, target);
-        });
+          cachedDocument.body.innerHTML = xhr.responseText, xhr._cachedTarget = {}), // clear the xhr embeds list and embed each item
+          xhr._embeds.splice(0).map(function (item) {
+            // get the cached target
+            var target = xhr._cachedTarget[item.id];
+            // ensure the cached target
+            target || (target = xhr._cachedTarget[item.id] = cachedDocument.getElementById(item.id)),
+              // embed the target into the svg
+              embed(item.parent, item.svg, target);
+          });
       }
     }, // test the ready state change immediately
-    xhr.onreadystatechange();
+      xhr.onreadystatechange();
   }
   function svg4everybody(rawopts) {
     function oninterval() {
       // while the index exists in the live <use> collection
       for (// get the cached <use> index
-      var index = 0; index < uses.length; ) {
+        var index = 0; index < uses.length;) {
         // get the current <use>
         var use = uses[index], parent = use.parentNode, svg = getSVGAncestor(parent), src = use.getAttribute("xlink:href") || use.getAttribute("href");
         if (!src && opts.attributeName && (src = use.getAttribute(opts.attributeName)),
-        svg && src) {
+          svg && src) {
           if (polyfill) {
             if (!opts.validate || opts.validate(src, svg, use)) {
               // remove the <use> element
@@ -67,13 +67,13 @@
                 var xhr = requests[url];
                 // ensure the xhr request exists
                 xhr || (xhr = requests[url] = new XMLHttpRequest(), xhr.open("GET", url), xhr.send(),
-                xhr._embeds = []), // add the svg and id as an item to the xhr embeds list
-                xhr._embeds.push({
-                  parent: parent,
-                  svg: svg,
-                  id: id
-                }), // prepare the xhr ready state change event
-                loadreadystatechange(xhr);
+                  xhr._embeds = []), // add the svg and id as an item to the xhr embeds list
+                  xhr._embeds.push({
+                    parent: parent,
+                    svg: svg,
+                    id: id
+                  }), // prepare the xhr ready state change event
+                  loadreadystatechange(xhr);
               } else {
                 // embed the local id into the svg
                 embed(parent, svg, document.getElementById(id));
@@ -99,7 +99,7 @@
     polyfill && oninterval();
   }
   function getSVGAncestor(node) {
-    for (var svg = node; "svg" !== svg.nodeName.toLowerCase() && (svg = svg.parentNode); ) {}
+    for (var svg = node; "svg" !== svg.nodeName.toLowerCase() && (svg = svg.parentNode);) { }
     return svg;
   }
   return svg4everybody;
